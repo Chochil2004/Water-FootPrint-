a899aedbc00abdaa10ff7155f03c6df812c4765e
rivaemil <rivasm.emiliano@gmail.com>
2024-08-21 12:52:02 -0600

    Editor de preguntas añadido

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,13 +5,13 @@
         </h2>
     </x-slot>
 
-    {{-- <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div> --}}
+            <a href="http://127.0.0.1:8000/Preguntas" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                <div>
+                    <h2 class="mt-6 text-xl font-semibold text-white-900 dark:text-white">Administrar preguntas</h2>
+                        <p class="mt-4 text-white-500 dark:text-white-400 text-sm leading-relaxed">
+                            Administra las preguntas en el simulador
+                        </p>
+                    </div>
+            </a>
+
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/Preguntas', function () {
+    return view('agregar');
+});
+
 require __DIR__.'/auth.php';
