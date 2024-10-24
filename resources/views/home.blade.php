c936746f2337dd9ce54829f987e753c5985c34d8
josjua1610 <josephjua1610@gmail.com>
2024-10-24 10:25:53 -0600

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,6 +19,7 @@
             </label>
         </nav>
         <div class="content ">
+            <h1>prueba</h1>
             <h1>CALCULA TU HUELLA HIDRICA</h1>
             <div>
                 <a href="http://127.0.0.1:8000/p1"> <button id="startButton">INICIO</button></a>
